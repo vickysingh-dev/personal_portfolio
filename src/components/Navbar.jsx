3f269ca482d977acf82358921dffe1d16edbda7c
Vicky Singh <vickyawakinn@gmail.com>
2023-03-09 23:15:23 +0530

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
 import "./css/navbar.css";
 
 export default function Navbar() {
+    function toggleMenu(e) {
+        console.log("btn pressed");
+        document.querySelector(".nav-list").style.transform = "translateX(0)";
+    }
+
+    useEffect(() => {
+        document
+            .querySelector(".menu-btn")
+            .addEventListener("click", toggleMenu);
+        return document
+            .querySelector(".menu-btn")
+            .removeEventListener("click", toggleMenu);
+    }, []);
+
     return (
         <div className="navbar">
             <nav>
@@ -21,6 +36,11 @@ export default function Navbar() {
                         </a>
                     </li>
                 </ul>
+                <div className="menu-btn close" onClick={toggleMenu}>
+                    <div className="slant top"></div>
+                    <div className="slant middle"></div>
+                    <div className="slant bottom"></div>
+                </div>
             </nav>
         </div>
     );
